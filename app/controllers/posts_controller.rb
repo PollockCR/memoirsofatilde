95b16079be0041f3bd088b4bbcf6040d91fb8fb6
Catherine Pollock <pollockcr@gmail.com>
2018-12-23 09:48:37 -1000

    Set enviornment variables

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 
   before_action :find_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate, only: [:admin, :new, :create, :edit, :update, :destroy]
 
   def index
     if params[:category].blank?
@@ -44,6 +45,10 @@ class PostsController < ApplicationController
     redirect_to root_path, notice: "Post deleted~"
   end
 
+  def admin
+    redirect_to root_path if authenticate
+  end
+
 private
 
   # mark params as required
@@ -53,6 +58,16 @@ private
 
   def find_post
     @post = Post.find(params[:id])
+  end
+
+protected
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      admin_username = Rails.application.secrets.admin_username
+      admin_password = Rails.application.secrets.admin_password
+      username == admin_username && password == admin_password
+    end
   end
 
 end
